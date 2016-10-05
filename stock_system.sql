8cb3ad1f3f0d8525ba5b4b9df63001a0abb49391
whadever <irpanwinata@gmail.com>
2016-10-05 18:13:10 +0700

    the DB

--- stock_system.sql
+++ stock_system.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 27, 2016 at 02:56 AM
+-- Generation Time: Oct 05, 2016 at 11:12 AM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 7.0.6
 
@@ -23,12 +23,57 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'Beverages');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `code` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `category` int(11) NOT NULL,
+  `buying_price` double NOT NULL,
+  `selling_price` double NOT NULL DEFAULT '0',
+  `photo` varchar(255) NOT NULL,
+  `thumb` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`code`, `name`, `category`, `buying_price`, `selling_price`, `photo`, `thumb`) VALUES
+('BV000001', 'Green Tea', 1, 5000, 0, '', ''),
+('BV000002', 'Milk Tea', 1, 7000, 0, '', ''),
+('BV000003', 'test', 1, 10000, 0, 'uploads/BV000003/Black-Logo-Batman-wallpapers.jpg', 'uploads/BV000003/Black-Logo-Batman-wallpapers_thumb.jpg'),
+('BV000004', 'Alazka Tea', 1, 8000, 0, '', ''),
+('BV000005', 'Bro', 1, 9000, 0, 'uploads/BV000005/ironman_desktop.jpg', 'uploads/BV000005/ironman_desktop_thumb.jpg');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `role` enum('admin','manager','staff') NOT NULL,
+  `role` enum('admin','outlet') NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -44,15 +89,27 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `role`, `username`, `password`, `name`, `email`, `address`, `phone_number`, `mobile_number`, `photo`) VALUES
-(1, 'admin', 'irvan', '2beb367e8f1ae90320a4268d7c84a68903be0248acdd63788206d415a7fcbc621279caf5052d7bbe126a0cf3920485b77307d5142eb6402a95ae2cad43298239', 'Irvan Winata', 'irvan@gethassee.com', 'Gading Nirwana blok PF 17 no 11, Kelapa Gading, Jakarta Utara, 14240', '+6281316878995', '+6281316878995', 'uploads/photos/irvan/minion.png'),
-(2, 'manager', 'setyawan', 'bccbd5d823fc568cbae5a82069d48c46f36ab27ed0f629af8cb739d984c1e4ea6c71888496d4c42018427401a83043eeae8707de4fdcd0a7108fa5b8996d9b06', 'Setyawan Susanto', 'setyawan@gethassee.com', '', '', '', ''),
-(3, 'manager', 'John', 'bccbd5d823fc568cbae5a82069d48c46f36ab27ed0f629af8cb739d984c1e4ea6c71888496d4c42018427401a83043eeae8707de4fdcd0a7108fa5b8996d9b06', 'Jonathan Hosea', 'jonathan@gethassee.com', '', '', '', ''),
-(4, 'staff', 'felita', 'bccbd5d823fc568cbae5a82069d48c46f36ab27ed0f629af8cb739d984c1e4ea6c71888496d4c42018427401a83043eeae8707de4fdcd0a7108fa5b8996d9b06', 'Felita Setiawan', 'felita@gethassee.com', '', '', '', ''),
-(5, 'admin', 'reyner', 'bccbd5d823fc568cbae5a82069d48c46f36ab27ed0f629af8cb739d984c1e4ea6c71888496d4c42018427401a83043eeae8707de4fdcd0a7108fa5b8996d9b06', 'Gerald Reyner Liando', 'reyner@gethassee.com', '', '', '', '');
+(1, 'admin', 'irvan', 'bccbd5d823fc568cbae5a82069d48c46f36ab27ed0f629af8cb739d984c1e4ea6c71888496d4c42018427401a83043eeae8707de4fdcd0a7108fa5b8996d9b06', 'Irvan Winata', 'irvan@gethassee.com', 'Gading Nirwana blok PF 17 no 11, Kelapa Gading, Jakarta Utara, 14240', '+6281316878995', '+6281316878995', 'uploads/photos/irvan/photo.jpg'),
+(2, 'outlet', 'setyawan', 'bccbd5d823fc568cbae5a82069d48c46f36ab27ed0f629af8cb739d984c1e4ea6c71888496d4c42018427401a83043eeae8707de4fdcd0a7108fa5b8996d9b06', 'Setyawan Susanto', 'setyawan@gethassee.com', 'Apartment Puri Park View', '', '', 'uploads/photos/setyawan/photo.jpg'),
+(3, 'outlet', 'John', 'bccbd5d823fc568cbae5a82069d48c46f36ab27ed0f629af8cb739d984c1e4ea6c71888496d4c42018427401a83043eeae8707de4fdcd0a7108fa5b8996d9b06', 'Jonathan Hosea', 'jonathan@gethassee.com', '', '', '', ''),
+(4, 'outlet', 'felita', 'bccbd5d823fc568cbae5a82069d48c46f36ab27ed0f629af8cb739d984c1e4ea6c71888496d4c42018427401a83043eeae8707de4fdcd0a7108fa5b8996d9b06', 'Felita Setiawan', 'felita@gethassee.com', '', '', '', 'uploads/photos/felita/photo.jpg'),
+(5, 'admin', 'reyner', 'bccbd5d823fc568cbae5a82069d48c46f36ab27ed0f629af8cb739d984c1e4ea6c71888496d4c42018427401a83043eeae8707de4fdcd0a7108fa5b8996d9b06', 'Gerald Reyner Liando', 'reyner@gethassee.com', '', '', '', 'uploads/photos/reyner/photo.jpg');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`code`);
 
 --
 -- Indexes for table `users`
@@ -64,6 +121,11 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `users`
 --
