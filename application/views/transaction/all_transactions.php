f903fe8039655f3c18f5e060d2f73f2ee81ec429
felitas <felita.31895@gmail.com>
2016-10-19 11:25:56 +0700

    tampilans

--- application/views/transaction/all_transactions.php
+++ application/views/transaction/all_transactions.php
@@ -28,11 +28,11 @@
 				<label for="">Search :</label>
 				<input type="text" class="form-control" id="filter">
 				<?php if($role != 'admin'): ?>
-					<a href="<?php echo base_url('products/add') ?>" class="btn btn-primary pull-right">Transaksi Baru</a>
+					<a href="<?php echo base_url('selling') ?>" class="btn btn-primary pull-right">Transaksi Baru</a>
 				<?php endif; ?>
 			</div>
 			<div class="table-responsive toggle-circle-filled">
-			<table class="table table-condensed" data-filter="#filter" data-page-size="10" id="table_product">
+			<table class="table table-condensed table_product" data-filter="#filter" data-page-size="10" >
 				<thead>
 					 <tr>
 					 	<th data-type="numeric" data-sort-initial="true">No</th>
@@ -80,9 +80,124 @@
 				</tfoot>
 			</table>
 		</div>
-		
-		
-	
+	</div>
+	<div class="row" style="margin-top: 20px;">
+		<p class="bebas">PEMBELIAN</p>
+			<div class="form-group" style="margin-bottom: 20px">
+				<label for="">Search :</label>
+				<input type="text" class="form-control" id="filter">
+				<?php if($role != 'admin'): ?>
+					<a href="<?php echo base_url('selling') ?>" class="btn btn-primary pull-right">Transaksi Baru</a>
+				<?php endif; ?>
+			</div>
+			<div class="table-responsive toggle-circle-filled">
+			<table class="table table-condensed table_product" data-filter="#filter" data-page-size="10" >
+				<thead>
+					 <tr>
+					 	<th data-type="numeric" data-sort-initial="true">No</th>
+					 	<th data-hide="phone">Kode</th>
+					 	<th data-toggle="true">Tanggal</th>
+					 	<th data-type="numeric">Nama Barang</th>
+					 	<th data-hide="phone">Kuantitas</th>
+					 	<th data-type="numeric" data-hide="phone">Supplier</th>
+					 	<th data-type="numeric" data-hide="phone">Detail</th>
+					 	<?php if($role != 'admin'): ?>
+					 		<th data-hide="phone">Tindakan</th>
+						<?php else: ?>
+						 	<th data-hide="phone">Lokasi</th>
+						<?php endif; ?>
+					 </tr>
+				</thead>
+				<tbody>
+					<?php $i=1; foreach($sellings as $selling): ?>
+						<tr>
+							<td><?php echo $i ?></td>
+							<td><?php echo $selling->code ?></td>
+							<td><?php echo $selling->name ?></td>
+							<td><?php echo $product->model ?></td>
+							<td><?php echo $product->category ?></td>
+							<td><?php echo rupiah($product->buying_price) ?></td>
+							<td><?php echo rupiah($product->selling_price) ?></td>
+							<td>
+							<?php echo ($product->quantity==0)? '<p class="nostock">Out of stock</p>':'Available' ?>	
+							</td>
+							<td><a class="fancybox" rel="group" href="<?php echo base_url().$product->photo ?>"><img src="<?php echo base_url().$product->thumb ?>" alt="<?php echo $product->name ?>" width="40"/></a></td>
+							<?php if($role != 'admin'): ?>
+						 		<td><a href="<?php echo base_url('products/edit_product').'/'.$product->code?>">edit</a> | delete</td>
+							<?php else: ?>
+							 	<td><?php echo $product->outlet_name ?></td>
+							<?php endif; ?>
+							
+						</tr>
+					<?php $i++; endforeach; ?>
+				</tbody>
+				<tfoot class="hide-if-no-paging">
+					<td colspan="9">
+						<div class="pagination"></div>
+					</td>
+					
+				</tfoot>
+			</table>
+		</div>
+	</div>
+	<div class="row" style="margin-top: 20px;">
+		<p class="bebas">MUTASI</p>
+			<div class="form-group" style="margin-bottom: 20px">
+				<label for="">Search :</label>
+				<input type="text" class="form-control" id="filter">
+				<?php if($role != 'admin'): ?>
+					<a href="<?php echo base_url('selling') ?>" class="btn btn-primary pull-right">Transaksi Baru</a>
+				<?php endif; ?>
+			</div>
+			<div class="table-responsive toggle-circle-filled">
+			<table class="table table-condensed table_product" data-filter="#filter" data-page-size="10">
+				<thead>
+					 <tr>
+					 	<th data-type="numeric" data-sort-initial="true">No</th>
+					 	<th data-hide="phone">Kode</th>
+					 	<th data-toggle="true">Tanggal</th>
+					 	<th data-type="numeric">Nama Barang</th>
+					 	<th data-hide="phone">Kuantitas</th>
+					 	<th data-type="numeric" data-hide="phone">Outlet</th>
+					 	<th data-type="numeric" data-hide="phone">Detail</th>
+					 	<?php if($role != 'admin'): ?>
+					 		<th data-hide="phone">Tindakan</th>
+						<?php else: ?>
+						 	<th data-hide="phone">Lokasi</th>
+						<?php endif; ?>
+					 </tr>
+				</thead>
+				<tbody>
+					<?php $i=1; foreach($sellings as $selling): ?>
+						<tr>
+							<td><?php echo $i ?></td>
+							<td><?php echo $selling->code ?></td>
+							<td><?php echo $selling->name ?></td>
+							<td><?php echo $product->model ?></td>
+							<td><?php echo $product->category ?></td>
+							<td><?php echo rupiah($product->buying_price) ?></td>
+							<td><?php echo rupiah($product->selling_price) ?></td>
+							<td>
+							<?php echo ($product->quantity==0)? '<p class="nostock">Out of stock</p>':'Available' ?>	
+							</td>
+							<td><a class="fancybox" rel="group" href="<?php echo base_url().$product->photo ?>"><img src="<?php echo base_url().$product->thumb ?>" alt="<?php echo $product->name ?>" width="40"/></a></td>
+							<?php if($role != 'admin'): ?>
+						 		<td><a href="<?php echo base_url('products/edit_product').'/'.$product->code?>">edit</a> | delete</td>
+							<?php else: ?>
+							 	<td><?php echo $product->outlet_name ?></td>
+							<?php endif; ?>
+							
+						</tr>
+					<?php $i++; endforeach; ?>
+				</tbody>
+				<tfoot class="hide-if-no-paging">
+					<td colspan="9">
+						<div class="pagination"></div>
+					</td>
+					
+				</tfoot>
+			</table>
+		</div>
 	</div>
 </div>
 <!-- <div class="modal fade" id="delete">
@@ -104,7 +219,7 @@
 
 <script>
 	$(document).ready(function() {
-   	 $('#table_product').footable();
+   	 $('.table_product').footable();
    	 $(".fancybox").fancybox();
 	} );
 </script>
