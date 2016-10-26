ab8dbbaf8e9c6b05f60fb05500f147f158503643
felitas <felita.31895@gmail.com>
2016-10-26 17:11:07 +0700

    ga muncul

--- application/controllers/Mutasi.php
+++ application/controllers/Mutasi.php
@@ -38,6 +38,69 @@ class Mutasi extends MY_Controller
 		$data['drivers'] = $this->crud_model->get_data('drivers')->result();
 		$this->template->load('default','mutasi/send',$data);		
 	}
+
+	public function get_barcode($barcode){
+		if($this->db->get_where('products',array('code'=>$barcode))->num_rows() > 0){
+
+			echo '<script>$(".no-item").remove()</script>';
+			$product = $this->db->get_where('products',array('code'=>$barcode))->row();
+			$category = $this->crud_model->get_by_condition('category',array('id'=>$product->category))->row();
+			$specifications = $this->crud_model->get_by_condition('spesifikasi',array('kode_barang'=>$product->code))->result();
+			//$customers = $this->crud_model->get_data('customers')->result();
+			echo ($product->quantity == 0)? '<div class="text-center"><p class="bebas" style="color:red">Out of Stock</p></div>': null;
+			echo '<style>#table-detail>tbody>tr>th, #table-detail>tfoot>tr>td, #table-detail>tfoot>tr>th, #table-detail>thead>tr>td, #table-detail>thead>tr>th {padding-left: 0px !important;}</style>';
+			echo '<table class="table" id="table-detail"><tbody><tr><td class="tabhead"><p class="bebas">Nama Barang</p></td>';
+			echo '<td><p class="bebas">'.$product->name.'</p></td</tr>';
+			
+			
+			echo '</select></td></tr>';
+			echo '<tr><td class="tabhead"><p class="bebas">Kategori</p></td>';
+			echo '<td> <p class="bebas">'.$category->name.'</p></td></tr>';
+			echo '<tr><td class="tabhead"><p class="bebas">Kode Model</p></td>';
+			echo '<td> <p class="bebas">'.$product->model.'</p></td></tr>';
+			echo '<tr><td class="tabhead"><p class="bebas">Spesifikasi</p></td><td><ul  style="padding-left:6px;">';
+
+			foreach ($specifications as $specification) {
+				echo '<li> <p class="bebas">'.$specification->spec.'</p></li>';
+			}
+			echo '</ul></td></tr>';
+			
+
+			echo '<tr><td><p class="bebas">Harga Jual</p></td>';
+			echo '<td> <p class="bebas">$&nbsp;'.$product->selling_price.'</p></td></tr>';
+
+			
+			
+			echo '</tbody></table>';	
+
+
+			
+
+		}
+		else{
+			echo '<script>$(".no-item").remove()</script>';
+			//echo '<div class="text-center"><p class="bebas">Item not found</p></div>';
+			echo '<script>$(".product-table>tbody").append("<tr id=\"found\"><td colspan=\"2\"><div class=\"text-center\"><p class=\"bebas\" id=\"not_found\">Item not found</p></div></td></tr>")</script>';
+			echo '<script>$("#detail").append("<div class=\"text-center no-item\" style=\"margin-top: 20PX;\"><P class=\"bebas\" style=\"color:red\">PRODUK TIDAK ADA</P></div>")</script>';
+		}
+	}
+	public function list_item($barcode){
+		if($this->db->get_where('products',array('code'=>$barcode))->num_rows() > 0){
+			$product = $this->db->get_where('products',array('code'=>$barcode))->row();
+			$customers = $this->crud_model->get_data('customers')->result();
+			
+			if($this->db->get_where('cart',array('product_code'=>$barcode))->num_rows() > 0){
+				echo 'added';
+			}else{
+				$this->db->insert('cart',array('product_code' => $barcode, 'quantity' => 1));
+					if($product->quantity>0){
+					echo json_encode($product);
+
+				}
+			}
+			
+		}
+	}
 }
 
 
--- application/views/mutasi/send.php
+++ application/views/mutasi/send.php
@@ -4,84 +4,162 @@
 	}
 </style>
 		<div class="row content-wrap" style="margin-top: 20px;">
-		<?php echo form_open_multipart('products/distribute') ?>
-			<table class="table distribute-table">
-				<tbody>
-				<tr>
-					<td colspan="2"><p class="bebas">Kode Transaksi</p></td>
-					
-				</tr>
-				<tr>
-					<td colspan="2"><input type="text" name="transaction_code" placeholder="Kode Transaksi" class="form-control" required="1"></td>
-				</tr>
-				<tr>
-					<td><p class="bebas">PILIH OUTLET</p></td>
-					<td><p class="bebas">PILIH DRIVER</p></td>
-				</tr>
-				<tr>
-					<td>
-						<select required="1" class="form-control" id="" name="outlet_distribute">
-						<option value="">--Pilih Outlet--</option>
-						<?php foreach($outlets as $outlet): ?>
-							<option value="<?php echo $outlet->id ?>"><?php echo $outlet->username ?></option>
-						<?php endforeach; ?>
-						</select>
-					</td>
-					<td>
-						<select required="1" class="form-control" id="" name="outlet_distribute">
-						<option value="">--Pilih Driver--</option>
-						<?php foreach($drivers as $driver): ?>
-							<option value="<?php echo $driver->code ?>"><?php echo $driver->name ?></option>
-						<?php endforeach; ?>
-						</select>
-					</td>
-				</tr>
-				<tr>
-					<td colspan="2"><p class="bebas">Daftar Barang</p></td>
-				</tr>
-				<tr>
-					<td colspan="2"><input type="text" name="product_code" placeholder="Kode Barang" class="form-control" required="1"></td>
-				</tr>
-				<!-- <tr>
-					<td class="table_detail" colspan="2" style="padding-bottom: 0px;"><p class="bebas">PRODUK YANG DIKIRIM</p></td>
-				</tr> -->
-				<!-- <tr>
-					<td colspan="2">
-					<table class="table product-table" id="product_table" style="background: transparent;margin-bottom: 0px;">
+			<div class="col-md-12">
+				<div class="row">
+					<?php echo form_open_multipart('products/distribute') ?>
+					<table class="table distribute-table">
 						<tbody>
-							<tr>
-							<td style="padding-left: 0px;">
-								<select required="1" class="form-control" id="" name="product_distribute[]">
-									<option value="">--Pilih Produk--</option>
-									<?php foreach($products as $product): ?>
-										<option value="<?php echo $product->code ?>"><?php echo $product->name ?></option>
-									<?php endforeach; ?>
+						<tr>
+							<td colspan="2"><p class="bebas">Kode Transaksi</p></td>
+							
+						</tr>
+						<tr>
+							<td colspan="2"><input type="text" name="transaction_code" placeholder="Kode Transaksi" class="form-control" required="1"></td>
+						</tr>
+						<tr>
+							<td><p class="bebas">PILIH OUTLET</p></td>
+							<td><p class="bebas">PILIH DRIVER</p></td>
+						</tr>
+						<tr>
+							<td>
+								<select required="1" class="form-control" id="" name="outlet_distribute">
+								<option value="">--Pilih Outlet--</option>
+								<?php foreach($outlets as $outlet): ?>
+									<option value="<?php echo $outlet->id ?>"><?php echo $outlet->username ?></option>
+								<?php endforeach; ?>
 								</select>
 							</td>
-							<td style="padding-right: 0px;"><input type="text" name="product_quantity[]" placeholder="Jumlah Barang" class="form-control" required="1"></td>
-							</tr>
+							<td>
+								<select required="1" class="form-control" id="" name="outlet_distribute">
+								<option value="">--Pilih Driver--</option>
+								<?php foreach($drivers as $driver): ?>
+									<option value="<?php echo $driver->code ?>"><?php echo $driver->name ?></option>
+								<?php endforeach; ?>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<td colspan="2"><p class="bebas">Daftar Barang</p></td>
+						</tr>
+						<tr>
+							<td colspan="2"><input type="text" name="item_code" id="item_code" placeholder="Kode Barang" class="form-control" required="1" onblur="get_barcode()"></td>
+						</tr>
 						</tbody>
-					</table>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<a href="#" class="btn btn-primary product-add">Tambah Barang</a>
-					</td>
-				</tr> -->
-				</tbody>
-			</table>
+					</table>	
+				</div>
+				<div class="row">
+					<div class="col-md-6" id="buyrow">
+						<div class="row text-center">
+							<P class="bebas" style="margin-top: 10px">PRODUK YANG DIBELI</P>
+						</div>
+						<?php echo form_open('selling/sell_product') ?>
+						<div class="table-responsive toggle-circle-filled">
+						<table class="table table-condensed table-mutation" data-filter="#filter" data-page-size="10" >
+
+							<thead><tr>
+								<th data-sort-initial="true" data-toggle="true">Nama</th>
+								<th data-type="numeric">Qty.</th>
+								<th data-hide="phone" data-type="numeric">Harga</th>
+								<th data-hide="phone"></th>
+							</tr></thead>
+							<?php $i=1;?>
+							<tbody id="item_list"><tr class="no-item">
+								
+					 			</tr>
+							</tbody>
+						</table>
+						<p>
+						
+						
+						</p>
+						<input type="hidden" name="total_harga" id="total_harga" value="">
+						
+						</div>
+						<div class="row text-center">
+							<input type="submit" name="next" class="btn btn-primary" value="Next">
+						</div>
+						
+					</div>
+					<div class="col-md-6" id="detailrow">
+						<div class="row text-center">
+							<P class="bebas" style="margin-top: 10px">DETAIL PRODUK</P>
+						</div>
+						<div class="row text-center no-item" style="margin-top: 20PX;">
+							<P class="bebas" style="color:red">BELUM ADA BARANG</P>
+						</div>
+						<div class="row" id="detail"></div>
+					</div> 
+				</div>
+			</div>
 		</div>
 		
 <script>
-	jQuery(function(){
-    	var counter = 1;
-    	jQuery('a.product-add').click(function(event){
-	        event.preventDefault();
+	// jQuery(function(){
+ //    	var counter = 1;
+ //    	jQuery('a.product-add').click(function(event){
+	//         event.preventDefault();
 
-	        var newRow = jQuery('<tr><td style="padding-left: 0px;"><select required="1" class="form-control" id="" name="product_distribute[]"><option>--Pilih Barang--</option></select></td><td style="padding-right: 0px;"><input name="product_quantity[]" placeholder="Jumlah Barang" class="form-control" required="1"/></td></tr>');
-	        jQuery('table#product_table').append(newRow);
+	//         var newRow = jQuery('<tr><td style="padding-left: 0px;"><select required="1" class="form-control" id="" name="product_distribute[]"><option>--Pilih Barang--</option></select></td><td style="padding-right: 0px;"><input name="product_quantity[]" placeholder="Jumlah Barang" class="form-control" required="1"/></td></tr>');
+	//         jQuery('table#product_table').append(newRow);
 
-    	});
+ //    	});
+	// });
+	$(document).ready(function(){
+		$('.table-mutation').footable();
 	});
+	function get_barcode(){
+		list_item();
+		var barcode = $('#item_code').val();
+		if (barcode == '') {	
+			
+			
+		}else{
+			
+			$('#found').remove();
+			$('#detail').empty();
+			$.ajax({
+				url:"<?php echo base_url('mutasi/get_barcode')?>" +'/'+ barcode,
+				type: "GET",
+				success : function(result){
+
+					$('#detail').append(result);
+					
+					$('#belum_ada').remove();
+					$('#item_code').val('');
+					$('#item_code').focus();
+
+				}
+			});
+
+		}
+	}
+	function list_item(){
+		var disc = $('')
+		var barcode = $('#item_code').val();
+		if(barcode==''){
+
+		}
+		else{
+			$.ajax({
+				url:"<?php echo base_url('mutasi/list_item')?>"+'/'+ barcode,
+				type:"GET",
+				success : function(result){
+					if(result == 'added'){
+
+					}else{
+						var test = JSON.parse(result);
+						var price = test.selling_price;
+						$('#item_list').append("<tr id='row_"+test.code+"' ><td>"+test.name+"</td><td>1</td><td id='harga_"+test.code+"'>Rp "+test.selling_price+"</td><td><a onclick='remove_item(\""+test.code+"\")' style='cursor: pointer'>&times;</a></td></tr><input type='hidden' name='id[]' value='"+test.code+"'><input type='hidden' name='disc_price[]' id='disc_price_"+test.code+"' value='"+test.selling_price+"'> ");
+						// $('#total_price').empty();
+						// $('#total_price').append('$&nbsp;'+total_price);
+						// $('#total_harga').val(total_price);
+					}
+						
+
+
+				}
+			})
+		}
+		
+	}
 </script>
