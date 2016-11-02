ada448aeb8e2ef67796c499980c1ca5c35dc9434
felitas <felita.31895@gmail.com>
2016-11-02 14:42:57 +0700

    rude

--- application/models/Transaction_model.php
+++ application/models/Transaction_model.php
@@ -10,7 +10,17 @@ class Transaction_model extends CI_Model{
 		$this->db->order_by('transaction.transaction_date','desc');
 
 		return $this->db->get()->result();
-	}	
+	}
+
+	function getPurchasing(){
+		$this->db->select('transaction.*,supplier.name');
+		$this->db->from('transaction');
+		$this->db->join('customers','customers.id=transaction.to_client_id');
+		$this->db->where('transaction.transaction_type_id',1);
+		$this->db->order_by('transaction.transaction_date','desc');
+
+		return $this->db->get()->result();	
+	}
 }
 
 ?>
